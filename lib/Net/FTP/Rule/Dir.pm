aed0680a2512b7a128f4ba5a0271d69c012c667d
Diab Jerius <djerius@cfa.harvard.edu>
2017-05-03 17:50:11 -0400

    bump version

--- lib/Net/FTP/Rule/Dir.pm
+++ lib/Net/FTP/Rule/Dir.pm
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 use experimental 'switch';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp;
 use Fcntl qw[ :mode ];
--- lib/Net/FTP/Rule/Entry.pm
+++ lib/Net/FTP/Rule/Entry.pm
@@ -8,7 +8,7 @@ use strict;
 use warnings;
 use experimental 'switch';
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Carp;
 use Fcntl qw[ :mode ];
